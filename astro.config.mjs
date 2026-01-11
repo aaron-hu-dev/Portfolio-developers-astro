e01d4470efbc1f20f79e9881dfef28df551754fb
Aaron Hu <aaron@aaronhu.dev>
2026-01-11 17:03:48 +0000

    feat: enhance icon integration and improve server configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,55 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  integrations: [
+    icon({
+      include: {
+        lucide: [
+          "rocket",
+          "atom",
+          "wind",
+          "server",
+          "earth",
+          "file-code",
+          "eclipse",
+          "philippine-peso",
+          "trending-up",
+          "camera",
+          "smartphone",
+          "cpu",
+          "code",
+          "video",
+          "cloud",
+          "flame",
+          "linkedin",
+          "github",
+          "mail",
+          "briefcase",
+          "folder-code",
+          "external-link",
+          "star",
+        ],
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
       exclude: ['@astrojs/node']
+    },
+    ssr: {
+      external: ['astro-icon'],
+      noExternal: []
     }
   },
 	server: {
 		allowedHosts:[
 			'aaronhu.dev'
-		]
+		],
+		middlewareMode: true,
+		hmr: {
+			timeout: 120000
+		}
 	},
 
 });
